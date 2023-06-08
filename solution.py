1e43dcb99d063da69f5dfe2a559301c327f13ed4
Udokaamos <udokaamos@gmail.com>
2023-06-09 00:17:43 +0200

    made changes to text inator

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
-text = input("Enter the word: ")
+word = input("Enter the word: ")
 text2 = "inator"
-text3 = len(text)
-text4 = 1000
+text3 = len(word)
+text4 = '000'
 text5 = "-inator"
-new_text = text + text2
-text6 = text3 * text4
-new_text2 = text + text5
+new_text = word + text2
+text6 = str(text3) + text4
+new_text2 = word + text5
 if text3 <= 8:
-    print(f'{text} -->  "{new_text}  {text6}"')
+    print(f'{word} ➞ "{new_text}  {text6}"')
 else:
-    print(f'{text} -->  "{new_text2} {text6}"')
+    print(f'{word} ➞  "{new_text2} {text6}"')
