3f544c103bb6e62811797386a4d8fcf9a9607a55
Udokaamos <udokaamos@gmail.com>
2023-06-09 08:01:17 +0200

    modified right arrow source code

--- solution.py
+++ solution.py
@@ -7,6 +7,6 @@ new_text = word + text2
 text6 = str(text3) + text4
 new_text2 = word + text5
 if text3 <= 8:
-    print(f'{word} ➞ "{new_text}  {text6}"')
+    print(f'{word}  \u2192  "{new_text}  {text6}"')
 else:
-    print(f'{word} ➞  "{new_text2} {text6}"')
+    print(f'{word}  \u2192  "{new_text2} {text6}"')
